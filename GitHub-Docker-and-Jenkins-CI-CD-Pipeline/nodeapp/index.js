d7f0533f73190942256f0d2497d6d4f52cc8a31a
K.A.C.W. Senevirathna <157800095+WishyCode@users.noreply.github.com>
2025-02-26 10:39:46 +0530

    Update index.js 4

--- GitHub-Docker-and-Jenkins-CI-CD-Pipeline/nodeapp/index.js
+++ GitHub-Docker-and-Jenkins-CI-CD-Pipeline/nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": "webhook test 3 Hello, Welcome to Adomic Arts samin" }');
+  res.send('{ "response": "webhook test 4 Hello, Welcome to Adomic Arts samin" }');
 });
 
 app.get("/will", function (req, res) {
